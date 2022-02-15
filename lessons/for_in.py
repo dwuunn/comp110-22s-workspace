71fa302dcc47774d46b47352598e04b84cfc6d25
Dun Wu <wuapu@ad.unc.edu>
2022-02-15 18:24:14 -0500

    LS18

---
+++ lessons/for_in.py
@@ -0,0 +1,16 @@
+"""An example of for in syntax."""
+
+names: list[str] = ["Dun", "Sili", "Lan", "Chengyi"]
+
+# Example of iterating through names using a while loop
+print("While loop:")
+i: int = 0
+while i < len(names):
+    name: str = names[i]
+    print(name)
+    i += 1
+
+# The following for..in loop is the same as the while loop 
+print("for..in output:")
+for name in names:
+    print(name)
