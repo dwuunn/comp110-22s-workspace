a4ec26830e728e6fbf28868e57033d6b0be74b23
Dun Wu <wuapu@ad.unc.edu>
2022-02-17 20:13:28 -0500

    EX05

--- exercises/ex05/utils.py
+++ exercises/ex05/utils.py
@@ -7,6 +7,7 @@ __author__ = "730393353"
 # Arguments: A list of integers.
 # Returns: A list of integers, containing only the even elements of the input parameter.
 def only_evens(xs: list[int]) -> list[int]:
+    """Return the even numbers in the list."""
     new_xs: list[int] = []
     i: int = 0
     while i < len(xs):
@@ -20,7 +21,14 @@ def only_evens(xs: list[int]) -> list[int]:
 # Parameters: A list and two ints, where the first int serves as a start index and the second int serves as an end index (not inclusive).
 # Returns: A List which is a subset of the given list, between the specified start index and the end index - 1.
 def sub(xs: list[int], a: int, b: int) -> list[int]:
+    """Return elements in a list from index a to b-1."""
     new_list: list[int] = []
+    if a < 0:
+        a = 0
+    if b > len(xs):
+        b = len(xs)
+    if len(xs) == 0 or a > len(xs) or b <= 0:
+        return new_list
     while a < b:
         new_list.append(xs[a])
         a += 1
@@ -31,6 +39,7 @@ def sub(xs: list[int], a: int, b: int) -> list[int]:
 # Parameters: Two lists of ints.
 # Returns: A list containing all elements of the first list, followed by all elements of the second list.
 def concat(xs: list[int], ys: list[int]) -> list[int]:
+    """Concatenate two lists."""
     a: int = len(xs)
     b: int = len(ys)
     i: int = 0
--- exercises/ex05/utils_test.py
+++ exercises/ex05/utils_test.py
@@ -7,28 +7,33 @@ from exercises.ex05.utils import only_evens, sub, concat
 
 
 def test_only_evens_empty() -> None:
+    """Empty list."""
     xs: list[int] = []
     assert only_evens(xs) == []
 
 
 def test_only_evens_use_fst() -> None:
+    """Test only_evens case 1."""
     xs: list[int] = [1, 2, 3]
     assert only_evens(xs) == [2]
 
 
 def test_only_evens_use_snd() -> None:
+    """Test only_evens case 2."""
     xs: list[int] = [4, 4, 4]
     assert only_evens(xs) == [4, 4, 4]
 
 
-def test_sub_empty() -> None:
+def test_sub_single() -> None:
+    """Empty list with incorrect indices."""
     xs: list[int] = []
-    a: int = 0
+    a: int = -1
     b: int = 0
     assert sub(xs, a, b) == []
 
 
 def test_sub_fst() -> None:
+    """Test sub case 1."""
     xs: list[int] = [10, 20, 30, 40]
     a: int = 1
     b: int = 3
@@ -36,6 +41,7 @@ def test_sub_fst() -> None:
 
 
 def test_sub_snd() -> None:
+    """Test sub case 2."""
     xs: list[int] = [37, 27, 17, 7, 0, -7]
     a: int = 3
     b: int = 4
@@ -43,18 +49,21 @@ def test_sub_snd() -> None:
 
 
 def test_concat_empty() -> None:
+    """Empty list."""
     xs: list[int] = []
     ys: list[int] = []
     assert concat(xs, ys) == []
 
 
 def test_concat_fst() -> None:
+    """Test concat case 1."""
     xs: list[int] = [1, 2, 3]
     ys: list[int] = [4, 5, 6]
     assert concat(xs, ys) == [1, 2, 3, 4, 5, 6]
 
 
 def test_concat_snd() -> None:
+    """Test concat case 1."""
     xs: list[int] = [7, 17, 27, 37, -7]
     ys: list[int] = [-17]
     assert concat(xs, ys) == [7, 17, 27, 37, -7, -17]
